dc5f522adcb7d6de6033f23e927639e8cbdfb5ad
elMolda <david97molano@gmail.com>
2020-02-06 14:34:40 -0500

    Token creation completed. Pending create tests folder and test with mock protected route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from resources.routes import initialize_routes
 from database.db import initialize_db
 from database.connStr import connStr
@@ -11,6 +12,8 @@ api = Api(app)
 bcrypt = Bcrypt(app)
 
 app.config['MONGODB_SETTINGS'] = connStr
+app.config['JWT_SECRET_KEY'] = 'super-secret'  
+jwt = JWTManager(app)
 
 initialize_db(app)
 initialize_routes(api)
--- resources/LogInResource.py
+++ resources/LogInResource.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restful import Resource
 from flask_bcrypt import check_password_hash
+from flask_jwt_extended import create_access_token
+import datetime
 from mongoengine.errors import DoesNotExist
 
 
@@ -16,7 +18,8 @@ class LoginEndpoint(Resource):
             authorized = self.check_password(password,user.password)
             if not authorized:
                 return {'Error': 'Email or password invalid'}, 401
-            return {'Login ok': 123456}, 200
+            access_token = self.create_jwt_token(user.id)
+            return {'token': access_token}, 200
         except DoesNotExist:
             return {'Error': 'Email or password invalid'}, 401
         except Exception:
@@ -24,3 +27,8 @@ class LoginEndpoint(Resource):
 
     def check_password(self,req_pass,usr_pass):
         return check_password_hash(usr_pass,req_pass)
+
+    def create_jwt_token(self,usr_id):
+        expires = datetime.timedelta(hours=0.1)
+        access_token = create_access_token(identity=str(usr_id), expires_delta=expires)
+        return access_token
