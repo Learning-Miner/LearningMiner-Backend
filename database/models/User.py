9e2dcd04c8b02cb812463ca306fa5ee75562425c
elMolda <david97molano@gmail.com>
2020-02-22 11:11:00 -0500

    Updated adding name and lastname when register

--- database/models/User.py
+++ database/models/User.py
@@ -1,5 +1,7 @@
 from ..db import db
 class User(db.Document):
     email = db.EmailField(required=True, unique=True)
+    name = db.StringField(required=True)
+    lastname = db.StringField(required=True)
     password = db.StringField(required=True, min_length=6)
     rol = db.StringField(required=True)
