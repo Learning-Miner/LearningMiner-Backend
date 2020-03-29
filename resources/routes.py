0de8cced8b4818fa3ed552c35208b8115714f18a
elMolda <david97molano@gmail.com>
2020-03-29 16:15:41 -0500

    Refactored ReportResource name

--- resources/routes.py
+++ resources/routes.py
@@ -1,7 +1,7 @@
 from .SignUpResource import SignupEndpoint
 from .LogInResource import LoginEndpoint
 from .ConceptMapResource import CreateConceptMapEndpoint, AlterConceptMapEndpoint, FilterUserConceptMapsEndpoint
-from .ReportGenerationResource import CreateReportsEndpoint, RetrieveReportsEndpoint
+from .ReportsResource import CreateReportsEndpoint, RetrieveReportsEndpoint
 def initialize_routes(api):   
     api.add_resource(LoginEndpoint, '/api/login')
     api.add_resource(SignupEndpoint, '/api/signup')
