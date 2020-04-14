4666a1b708096978c938b16ca6ef180865232b67
elMolda <david97molano@gmail.com>
2020-04-13 19:16:51 -0500

    Write base map to DB completed. Check text preprocessing pending

--- resources/ActivityResource.py
+++ resources/ActivityResource.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from database.models.Activity import Activity
 from .analytics.ActivityTextAnalytics import ActivityTextAnalytics
+from .ConceptMapResource import ConceptMapUtils
 
 class CreateActivityResource(Resource):
     @jwt_required
@@ -13,13 +14,13 @@ class CreateActivityResource(Resource):
             if user['rol'] == 'Student':
                 return {"Error":"Student cannot create activity"}, 401
             body = request.get_json()
-            ata = ActivityTextAnalytics(body['text'])
-            key_concepts, base_map_id = self.process_activity_text(ata)
-            #act = self.create_activity(body,user['id'],key_concepts,base_map_id)
-            #act.save()
-            return {"act_id":"yes"}, 201
+            ata = ActivityTextAnalytics(body['text'],body['title'])
+            key_concepts, base_map_id = self.process_activity_text(ata,user['id'])
+            act = self.create_activity(body,user['id'],key_concepts,base_map_id)
+            act.save()
+            return {"act_id":str(act.id)}, 201
         except Exception as e:
-            return {'Error': str(e)}, 500
+           return {'Error': str(e)}, 500
 
     def create_activity(self,body,uid,key_concepts,base_map_id):
         act = Activity(uid=uid)
@@ -27,12 +28,12 @@ class CreateActivityResource(Resource):
         act.text = body['text']
         act.dateClose = body['dateClose']
         act.title = body['title']
+        act.key_concepts = key_concepts
         return act
 
-    def process_activity_text(self,analytics):
-        key_concepts, base_map = analytics.process_activity_text()
-        print(key_concepts)
-        print(base_map)
-        #Create and save base map
-        base_map_id = 1 #base_map.save()
+    def process_activity_text(self,analytics,uid):
+        key_concepts, base_map_data = analytics.process_activity_text()
+        cmu = ConceptMapUtils()
+        base_map = cmu.create_concept_map(base_map_data,uid)
+        base_map_id = base_map.save()
         return key_concepts, base_map_id
--- resources/analytics/ActivityTextAnalytics.py
+++ resources/analytics/ActivityTextAnalytics.py
@@ -1,40 +1,43 @@
 from .TextAnalytics import TextAnalytics
 from random import randint
 class ActivityTextAnalytics:
-    def __init__(self,text):
+    def __init__(self,text,title):
         self.text = text
+        self.title = str(title)
         self.text_analytics = TextAnalytics()
     
     def process_activity_text(self):
         base_map = self.text_analytics.generate_base_map(self.text)
-        key_concepts = base_map.keys()
+        key_concepts = list(base_map.keys())
         base_map = self.base_map_json(base_map)
         return key_concepts, base_map
 
     def base_map_json(self,base_map):
         id_gen = 1
         json = dict()
+        json['isBase'] = True
+        json['title'] = self.title
         json['concepts'] = list()
         json['propositions'] = list()
         for key,val in base_map.items():
             concept_entry = dict()
-            concept_entry['text'] = key
-            concept_entry['id'] = id_gen
+            concept_entry['text'] = str(key)
+            concept_entry['id'] = str(id_gen)
             concept_entry['x'] = randint(300,1370)
             concept_entry['y'] = randint(142,624)
             json['concepts'].append(concept_entry)
             id_gen += 1
             for v in val:
                 concept_entry_2 = dict()
-                concept_entry_2['text'] = v
-                concept_entry_2['id'] = id_gen
+                concept_entry_2['text'] = str(v)
+                concept_entry_2['id'] = str(id_gen)
                 concept_entry_2['x'] = concept_entry['x'] + randint(100,150)
                 concept_entry_2['y'] = concept_entry['y'] + randint(100,150)
                 json['concepts'].append(concept_entry_2)
                 id_gen += 1
                 prop_entry = dict()
-                prop_entry['from'] = concept_entry['id']
-                prop_entry['to'] = concept_entry_2['id']
+                prop_entry['frm'] = str(concept_entry['id'])
+                prop_entry['to'] = str(concept_entry_2['id'])
                 prop_entry['text'] = ""
                 json['propositions'].append(prop_entry)
             
